3067fd88f8d6154411a41e434932e380ee1219db
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2021-08-06 00:03:25 +0100

    Create sort.py

---
+++ Lesson_three/sort.py
@@ -0,0 +1,14 @@
+"""
+ Sorting Algorithm is used to rearrange a given array or list elements according to a comparison operator on the elements.
+ 
+ Bubble sort, sometimes referred to as sinking sort, is a simple sorting algorithm that repeatedly steps through the list, 
+ compares adjacent elements and swaps them if they are in the wrong order. ... 
+ The algorithm, which is a comparison sort, is named for the way smaller or larger elements "bubble" to the top of the list
+ 
+[21, 4, 1, 3, 9, 20, 25, 6, 21, 14] (Original Array)
+[4, 1, 3, 9, 20, 21, 6, 21, 14, 25] (1)
+[1, 3, 4, 9, 20, 6, 21, 14, 21, 25] (2)
+[1, 3, 4, 9, 6, 20, 14, 21, 21, 25] (3)
+[1, 3, 4, 6, 9, 14, 20, 21, 21, 25] (4)
+
+"""
